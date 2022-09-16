0c30bd59db4b4a4fd654132239270cf48aed2ad3
spykelion <ndilionel6@gmail.com>
2022-09-16 08:25:18 +0100

    create room controller

---
+++ server/controllers/room.controller.js
@@ -0,0 +1,72 @@
+import Room from "../models/Room.js";
+
+
+async function getAllRooms(req, res) {
+  try {
+    await Room.find({})
+      .lean()
+      .then((result) => res.status(200).send(result))
+      .catch((err) => res.status(503).send(err));
+  } catch (error) {
+    return res
+      .send(501)
+      .json({ mrssage: "Server is Down" });
+  }
+}
+
+async function getOneRoom(req, res) {
+  try {
+    await Room.findOne({ _id: req.params.id })
+      .lean()
+      .then((result) => res.status(200).send(result))
+      .catch((err) => res.status(503).send(err));
+  } catch (error) {
+    return res
+      .send(501)
+      .json({ message: "Server is Down" });
+  }
+}
+
+async function createRoom(req, res) {
+  const {name, description, users} = req.body
+  try {
+    console.log("Init create Room");
+        const room = new Room({
+          name,
+          description,
+          users
+        })
+        room.save()
+        .then(saved=> {
+          return res.status(201).json(saved)
+        })
+      .catch((err) => console.error(err));
+  } catch (error) {
+    return res
+      .send(501)
+      .json({ message: "Server is Down" });
+  }
+}
+
+async function deleteOneRoom(req, res) {
+  try {
+    await Room.deleteOne({ _id: req.params.id })
+      .then((result) => res.status(200).send(result))
+      .catch((err) => res.status(503).send(err));
+  } catch (error) {
+    return res
+      .send(501)
+      .json({ message: "Server is Down" });
+  }
+}
+
+
+
+const room = {
+  getAllRooms,
+  getOneRoom,
+  createRoom,
+  deleteOneRoom,
+};
+
+export default room;
