e30c700425489f7d56bcbc230feb4c32e3ab25ed
spykelion <ndilionel6@gmail.com>
2022-09-16 23:41:30 +0100

    authenticate endpoints : message.routes : user.routes

--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import message from "../controllers/message.controller.js";
+import auth from '../auth/verify'
 const router = express.Router();
 
 router
-  .get("/", message.getAllMessages)
-  .post("/", message.createMessage)
-  .get("/:id", message.getOneMessage)
-  .delete("/:id", message.deleteOneMessage);
+  .get("/", auth, message.getAllMessages)
+  .post("/", auth, message.createMessage)
+  .get("/:id", auth, message.getOneMessage)
+  .delete("/:id", auth, message.deleteOneMessage);
 
 export default router;
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,22 +1,16 @@
-import multer from "multer";
 import express from "express";
 import user from "../controllers/user.controller.js";
 import uploadImage from "../controllers/fileUploader.js";
+import auth from "../auth/verify";
+import login from "../auth/auth.js";
 const router = express.Router();
 
-//handle http request for username and image for upload
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage });
-
 router
-  .get("/", user.getAllUsers)
+  .get("/login", login)
   .post("/", user.createUser)
-  .post('/avatar', user.uploadAvatar, uploadImage)
-  .get("/:id", user.getOneUser)
-  .delete("/:id", user.deleteOneUser);
-// app.post(
-//   "/api/upload",
-//   upload.single("avatar"),
-//   fileUploader
-// );
+  .post("/avatar", user.uploadAvatar, uploadImage)
+  .get("/", auth, user.getAllUsers)
+  .get("/:id", auth, user.getOneUser)
+  .delete("/:id", auth, user.deleteOneUser);
+
 export default router;
