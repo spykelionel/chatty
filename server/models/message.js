bd035538cf572b8f61c2915d32d68b6f911896ee
spykelion <ndilionel6@gmail.com>
2022-09-16 13:58:50 +0100

    fix schema object error

--- server/models/message.js
+++ server/models/message.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const messageSchema = new mongoose.Schema(
   {
     sender: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+        type: String,
         required: true,
       },
     body: {
