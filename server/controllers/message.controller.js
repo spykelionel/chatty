c22b5648fd8180167de0e7808b7e7ba391ae19f2
spykelion <ndilionel6@gmail.com>
2022-09-16 13:57:38 +0100

    fix send message bug

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,8 +1,71 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
+import socketIO, {Server} from "socket.io";
+
+// const io = socketIO(process.env.SOCKET_PORT, {
+//   cors: {
+//     origin: "http://localhost:1234",
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("Connection established");
+
+//   getAllMessages()
+//     .then((results) => {
+//       socket.emit("mostRecentMessages", results.reverse());
+//     })
+//     .catch((error) => {
+//       socket.emit("mostRecentMessages", []);
+//     });
+
+//   socket.on("newChatMessage", (data) => {
+//     //send event to every single connected socket
+//     const msgData = {
+//       sender: {
+//         user_name: data.name,
+//         user_avatar: data.avatar,
+//       },
+//       body: {
+//         text:data.message,
+//         attachment: data.attachment??''
+//       },
+//     };
+//     User.findOne({ name: sender.name })
+//       .then(async (user) => {
+//         const message = new Message({
+//           sender: user,
+//           body
+//         })
+//         message.save()
+//         .then(saved=> {
+//           io.emit("newChatMessage", saved)
+//           return res.status(201).json(saved)
+//         })
+//         // return res.status(200).json(sender)
+//       })
+//       .catch((err) => console.error(err));
+//     // try {
+//     //   const message = new Message(msgData);
+//     //   message
+//     //     .save()
+//     //     .then(() => {
+//     //       io.emit("newChatMessage", msgData);
+//     //     })
+//     //     .catch((error) => console.log("error: " + error));
+//     // } catch (e) {
+//     //   console.log("error: " + e);
+//     // }
+  
+//   });
+//   socket.on("disconnect", () => {
+//     console.log("connection disconnected");
+//   });
+// });
 
 
 async function getAllMessages(req, res) {
+ 
   try {
     await Message.find({})
       .lean()
@@ -10,8 +73,8 @@ async function getAllMessages(req, res) {
       .catch((err) => res.status(503).send(err));
   } catch (error) {
     return res
-      .send(501)
-      .json({ message: "Server is Down" });
+      .status(501)
+      .send({ message: "Server is Down" });
   }
 }
 
@@ -29,23 +92,32 @@ async function getOneMessage(req, res) {
 }
 
 async function createMessage(req, res) {
-  const {sender, body} = req.body
+  // const body = req.body
   try {
     console.log("Init create Message");
 
-    User.findOne({ _id: sender })
+    User.findOne({ name: req.body.sender })
       .then(async (user) => {
-        const message = new Message({
-          sender: user,
-          body
-        })
-        message.save()
-        .then(saved=> {
-          return res.status(201).json(saved)
-        })
-        // return res.status(200).json(sender)
+        if(user){
+          const message = new Message({
+            sender: user.name,
+            body: {
+              text: req.body.body.text
+            }
+          })
+          message.save()
+          .then(saved=> {
+            return res.status(201).json(saved)
+          })
+        }
+        else {
+          return res.status(404).json({message: "No user found"})
+        }
       })
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        console.error(err)
+        res.status(503).send({...err})
+      });
   } catch (error) {
     return res
       .send(501)
