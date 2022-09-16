3585ba59995dd4e346dc0cb21c4e9603aa7b375e
spykelion <ndilionel6@gmail.com>
2022-09-16 22:21:12 +0100

    little fix

--- server/auth/verify.js
+++ server/auth/verify.js
@@ -4,6 +4,7 @@ export default function(req, res, next){
     try {
         const token = req.headers['Authorization'].split(' ')[1]
         req.userData = jwt.verify(token, process.env.JWT_SECRET)
+        // next(req.userData)
         next()
     } catch (error) {
         return res.status(501).json({...error, message: "Error verifying user"})
