014ffd692ffd33e0cc28c49604d0111edbbf88ec
spykelion <ndilionel6@gmail.com>
2022-09-17 10:53:47 +0100

    update sendMessage : make form onsubmit

--- client/src/components/Chat/MessageBox/MessageBox.js
+++ client/src/components/Chat/MessageBox/MessageBox.js
@@ -1,65 +1,79 @@
-import React,{useState, useRef} from "react";
+import React, { useState, useRef } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import { Send } from "@material-ui/icons";
 
 //renaming prop for use in the component
 const MessageBox = (props) => {
   const [initialMessage] = useState({
-    sender: '',
-    avatar: '',
+    sender: "",
+    avatar: "",
     body: {
-      text: '',
-      attachment: ''
-    }
-  })
+      text: "",
+      attachment: "",
+    },
+  });
   const [message, setMessage] = useState(initialMessage);
   const messageRef = useRef("");
 
-  const sendMessageClick = () =>{
-    if(messageRef.current.value === ""){
+  const sendMessageClick = (e) => {
+    e.preventDefault();
+    if (messageRef.current.value === "") {
       return false;
     }
+    if (e.key === "Enter") {
+      props.onSendMessage(message);
+      setMessage(initialMessage);
+      return
+    }
+  
     props.onSendMessage(message);
     setMessage(initialMessage);
   }
+;
   const handleMessageChange = (e) => {
     setMessage({
-      sender: localStorage.getItem('user')??'anonymous',
-      avatar: '',
+      sender: localStorage.getItem("user") ?? "anonymous",
+      avatar: "",
       body: {
         text: messageRef.current.value,
-        attachment: ''
-      }
-    })
-  }
+        attachment: "",
+      },
+    });
+  };
 
   return (
-    <form className="chat-form" autoComplete="off">
-      <TextField
-        id="standard-basic"
-        label="Write your message here"
-        margin="normal"
-        multiline
-        fullWidth
-        minRows="4"
-        inputRef={messageRef}
-        onChange={handleMessageChange}
-        onKeyDown={event => {
-          if(event.key === "Enter"){
-            //prevents enter from being pressed
-            event.preventDefault();
-            sendMessageClick();
+    <form className="chat-form" onSubmit={sendMessageClick} autoComplete="off">
+      <div className="flex msg-box">
+        <TextField
+          id="standard-basic"
+          label="Write your message here"
+          margin="normal"
+          multiline
+          fullWidth
+          minRows="1"
+          inputRef={messageRef}
+          onChange={handleMessageChange}
+          value={message.body.text}
+          onKeyDown={(e)=>{
+            if (e.key === "Enter") {
+              props.onSendMessage(message);
+              setMessage(initialMessage);
+              return
+            }
           }
-        }}
-        value={message.body.text}
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={sendMessageClick}
-      >
-        Send
-      </Button>
+        }
+        />
+
+        <div className="control send-btn">
+          <button
+            className="button is-success"
+            type="submit"
+          >
+            <Send />
+          </button>
+        </div>
+      </div>
     </form>
   );
 };
