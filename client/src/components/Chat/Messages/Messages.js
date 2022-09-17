eaab088e3d16a6fe2fe0d501f90278e04defd8ac
spykelion <ndilionel6@gmail.com>
2022-09-17 10:54:33 +0100

    refactore Messages.js component : remove uneccessary material components

--- client/src/components/Chat/Messages/Messages.js
+++ client/src/components/Chat/Messages/Messages.js
@@ -1,39 +1,20 @@
-import React, { Fragment } from "react";
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from "@material-ui/core/Typography";
-
+import React from "react";
+import Message from "../../Message";
 
 const Messages = ({messages}) =>{
-  const current = localStorage.getItem('user')
+  const gotoLastMessageRef = React.useRef(null);
+  React.useEffect(() => {
+    gotoLastMessageRef.current.scrollIntoView({ behavior: "smooth" });
+}, [messages]);
+
   return (
-    <List>
-      {messages.flatMap((message, index) => [(
-        <ListItem alignItems="flex-start" key={index}>
-          <ListItemAvatar>
-            <Avatar alt="Avatar alt text" src={message.avatar} />
-          </ListItemAvatar>
-          <ListItemText
-            primary={message.sender}
-            secondary={
-              <Fragment>
-                <Typography
-                  component="span"
-                  variant="body2"
-                  color="textPrimary"
-                >
-                  <span className={(message.sender==current)&&"current"||''}>{message?.body?.text??''}</span>
-                </Typography>
-              </Fragment>
-            }
-          />
-        </ListItem>
-        ),<Divider variant="inset" component="li" key={"divider-"+index} />])}
-    </List>
+    <div className="p-max message-items">
+      {messages.map((message, index) => (
+        <Message message={message} key={index}/>
+        )
+      )}
+       <div ref={gotoLastMessageRef}></div>
+    </div>
   )
 };
 
