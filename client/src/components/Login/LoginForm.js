c6d97ade58e4cea51d15181a3262024d81080ab4
spykelion <ndilionel6@gmail.com>
2022-09-17 10:54:47 +0100

    refactore login form

--- client/src/components/Login/LoginForm.js
+++ client/src/components/Login/LoginForm.js
@@ -1,44 +1,44 @@
 import React, { useRef, useState } from "react";
 import { TextField, Button } from "@material-ui/core";
 import Loader from "react-loader-spinner";
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const LoginForm = ({ setUserDataForChat }) => {
   const [loading, setLoading] = useState(false);
-  const userNameInput = useRef("");
-  const passwordInputRef = useRef("")
-  const navigate = useNavigate()
-  const storage = window.localStorage
+  const userNameInputRef = useRef("");
+  const passwordInputRef = useRef("");
+  const navigate = useNavigate();
+  const storage = window.localStorage;
   async function authenticateUser(url, data) {
     fetch(url, {
-      method: 'post',
+      method: "post",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     })
-      .then(res=>res.json())
-      .then(data=>{
+      .then((res) => res.json())
+      .then((data) => {
         const user = {
           token: data.token,
-          user: data.user
-        }
-        console.log(user)
-        storage.setItem('user', user.user.name)
-        storage.setItem('token', user.token)
-        navigate('/')
+          user: data.user,
+        };
+        console.log(user);
+        storage.setItem("user", user.user.name);
+        storage.setItem("token", user.token);
+        navigate("/");
       })
-      .catch(err=>console.log(err))
+      .catch((err) => console.log(err));
   }
   const login = (e) => {
-    const url = 'http://localhost:5002/api/account/login'
-    e.preventDefault()
+    const url = "http://localhost:5002/api/account/login";
+    e.preventDefault();
     const data = {
-      name: userNameInput.current.value,
-      password: passwordInputRef.current.value
-    }
-    authenticateUser(url, data)
-  }
+      name: userNameInputRef.current.value,
+      password: passwordInputRef.current.value,
+    };
+    authenticateUser(url, data);
+  };
 
   return loading ? (
     <Loader
@@ -49,38 +49,61 @@ const LoginForm = ({ setUserDataForChat }) => {
     />
   ) : (
     <div>
-      <form className="login-form" onSubmit={login} autoComplete="off">
-      <TextField
-        id="chat-username"
-        label="Enter Username"
-        margin="normal"
-        fullWidth
-        minRows="1"
-        type="text"
-        inputRef={userNameInput}
-      />
-      <TextField
-        id="chat-password"
-        label="Enter Password"
-        margin="normal"
-        fullWidth
-        minRows="1"
-        type="password"
-        inputRef={passwordInputRef}
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        type="submit"
-      >
-        Enter Chat
-      </Button>
-      <div>
-        <p>
-          <Link to="/login">Signup</Link>
-        </p>
+      <div className="place-items-center w-40">
+        <div className="container content is-center">
+          <div className="p-6 has-background-light">
+            <h1 className="title has-text-centered has-text-success">
+              Chatty
+            </h1>
+            <h2 className="subtitle has-text-centered">
+              Login Form
+            </h2>
+            <form onSubmit={login}>
+              <div className="field">
+                <label className="label">Name</label>
+                <div className="control">
+                  <input
+                    name="name"
+                    type="text"
+                    id="name"
+                    placeholder="Enter name"
+                    className="input"
+                    ref={userNameInputRef}
+                  />
+                </div>
+              </div>
+
+              <div className="field">
+                <label className="label">Password</label>
+                <div className="control">
+                  <input
+                    name="password"
+                    type="password"
+                    id="password"
+                    ref={passwordInputRef}
+                    placeholder="Enter password"
+                    className="input"
+                  />
+                </div>
+              </div>
+
+              <div className="field is-grouped">
+                <div className="control">
+                  <button
+                    className="button is-success"
+                    type="submit"
+                  >
+                    Login
+                  </button>
+                </div>
+                <div className="control self-center">
+                  <Link to={"/signup"}>Register</Link>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-    </form>
     </div>
   );
 };
