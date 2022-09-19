a6fc722756811ddde18c0d0218209e6edfe16b66
spykelion <ndilionel6@gmail.com>
2022-09-19 05:11:47 +0100

    udate Chat.js : include Rooms

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,30 +2,39 @@ import React from "react";
 import MessageBox from "./MessageBox/MessageBox";
 import Messages from "./Messages/Messages";
 import useChat from "./useChat";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import Rooms from "./room/Rooms";
+import useRoom from "./room/useRoom";
 
-const Chat = (currentUserData) => {
+const Chat = () => {
   const navigate = useNavigate();
-  const { messages, sendMessage } = useChat();
+  // const _roomId = "6324234f7faf62cb8026bdfb"
+  const { roomId } = useParams()
+  console.log("Room ID:", roomId)
+  const {room} = useRoom(roomId)
+  const { messages, sendMessage } = useChat(roomId);
   const user = localStorage.getItem("user");
   if (!user) {
-    navigate("/login");
+    React.useEffect(()=>{
+      navigate("/login");
+    },[])
   }
   return (
     <div className="flex chat">
-      <div className="bg-green">{"chatty"}</div>
+      <div className="bg-green">
+      <Rooms />
+      </div>
       <div className="messages">
         <div className="p-2 has-background-light">
           <h1 className="title has-text-centered has-text-success">
             Chatty
           </h1>
           <h2 className="subtitle has-text-centered">
-            Public Room
+            {room.name??"Public"}
           </h2>
         </div>
         <Messages messages={messages} />
         <MessageBox
-          userData={currentUserData}
           onSendMessage={(message) => {
             sendMessage(message);
           }}
