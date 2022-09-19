eab32e50511505258e0a525187a6bc5ad7678199
spykelion <ndilionel6@gmail.com>
2022-09-19 23:19:19 +0100

    Update Room component

--- client/src/components/Chat/room/Room.js
+++ client/src/components/Chat/room/Room.js
@@ -3,9 +3,21 @@ import { Link } from 'react-router-dom'
 
 function Room({ room }) {
   return (
-    <div>
-        <Link to={`/room/${room._id}`}>{room.name}</Link>
-    </div>
+    <li>
+        <Link to={`/room/${room._id}`}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_06.jpg"
+            alt=""
+          />
+          <div>
+            <h2>{room.name}</h2>
+            <h3>
+              <span className="status green"></span>
+              <span>{'public room'}</span>
+            </h3>
+          </div>
+        </Link>
+    </li>
   )
 }
 
