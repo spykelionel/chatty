21ac56ad6a8851bfeec62d9de9f747ce600da93f
spykelion <ndilionel6@gmail.com>
2022-09-19 20:05:59 +0100

    update room.controller : update a single room, ad latestMessage object

--- server/controllers/room.controller.js
+++ server/controllers/room.controller.js
@@ -1,6 +1,5 @@
 import Room from "../models/Room.js";
 
-
 async function getAllRooms(req, res) {
   try {
     await Room.find({})
@@ -28,35 +27,34 @@ async function getOneRoom(req, res) {
 }
 
 async function createRoom(req, res) {
-  const {name, description, users} = req.body
-  Room.exists({name}).
-  then(room=>{
-    if(!room){
+  const { name, description, users } = req.body;
+  Room.exists({ name }).then((room) => {
+    if (!room) {
       try {
         console.log("Init create Room");
-            const room = new Room({
-              name,
-              description,
-              users
-            })
-            room.save()
-            .then(saved=> {
-              return res.status(201).json(saved)
-            })
+        const room = new Room({
+          name,
+          description,
+          users,
+        });
+        room
+          .save()
+          .then((saved) => {
+            return res.status(201).json(saved);
+          })
           .catch((err) => console.error(err));
       } catch (error) {
         return res
           .send(501)
           .json({ message: "Server is Down" });
       }
-    } else{
+    } else {
       return res.status(409).json({
         message: "Room already exists",
-        status: 409
-      })
+        status: 409,
+      });
     }
-  })
- 
+  });
 }
 
 async function deleteOneRoom(req, res) {
@@ -71,13 +69,42 @@ async function deleteOneRoom(req, res) {
   }
 }
 
-
+async function updateOneRoom(req, res) {
+  const { roomId } = req.params;
+  Room.exists({ _id: roomId }).then((exist) => {
+    if (!exist) {
+      return res
+        .status(404)
+        .json({ message: "Room not found", status: 404 });
+    }
+    Room.updateOne(
+      { _id: roomId },
+      { latestMessage: req.body.latestMessage },
+      { upsert: false }
+    )
+      .then((success) => {
+        return res.status(200).json({
+          ...success,
+          messsage: "Latest message updated",
+          status: 201,
+        });
+      })
+      .catch((err) => {
+        res.status(501).json({
+          ...err,
+          message: "Service unavailable or server is down",
+          status: 501,
+        });
+      });
+  });
+}
 
 const room = {
   getAllRooms,
   getOneRoom,
   createRoom,
   deleteOneRoom,
+  updateOneRoom,
 };
 
 export default room;
