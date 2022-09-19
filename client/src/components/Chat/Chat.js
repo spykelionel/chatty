e9947399d5312923f3ea63e8ce6ad62e314c357a
spykelion <ndilionel6@gmail.com>
2022-09-20 00:24:40 +0100

    update Chat component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,17 +2,14 @@ import React from "react";
 import MessageBox from "./MessageBox/MessageBox";
 import Messages from "./Messages/Messages";
 import useChat from "./useChat";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, Navigate } from "react-router-dom";
 import Rooms from "./room/Rooms";
 import useRoom from "./room/useRoom";
-import SidebarChat from "./SidebarChat";
-import Message from "../Message";
-import { Avatar } from "@material-ui/core";
-import { Send } from "@material-ui/icons";
+import Message from "./Messages/Message";
 
 const Chat = () => {
   const navigate = useNavigate();
-  // const _roomId = "6324234f7faf62cb8026bdfb"
+  const newRoomRef = React.useRef('');
   const { roomId } = useParams()
   console.log("Room ID:", roomId)
   const {room} = useRoom(roomId)
@@ -23,12 +20,37 @@ const Chat = () => {
       navigate("/login");
     },[])
   }
+  const storage = window.localStorage
+
+  const createNewRoom = (e) => {
+    e.preventDefault()
+    const url = `http://localhost:5002/api/room`
+    fetch(url, {
+      method: 'post',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: newRoomRef.current.value,
+        description: "A new room",
+      })
+    })
+    .then(res=>res.json())
+    .then(room=>{
+      newRoomRef.current.value = ''
+      window.location.reload()
+    })
+    .catch(err=>console.log(err))
+  }
   return (
     <div className="place-items-center">
     <div id="container">
     <aside>
       <header>
-        <input type="text" placeholder="search" />
+        <form onSubmit={createNewRoom}>
+          <input type="text" placeholder="create a new room" ref={newRoomRef} />
+          <button type="submit">Create room</button>
+        </form>
       </header>
     <Rooms />
     </aside>
@@ -52,18 +74,21 @@ const Chat = () => {
           messages.map((message, idx)=> <Message message={message} key={message._id} />)
         }
       </ul>
-      <footer>
-        <textarea placeholder="Type your message"></textarea>
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_picture.png"
-          alt=""
-        />
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_file.png"
-          alt=""
-        />
-        <Send />
-      </footer>
+     <MessageBox
+          onSendMessage={(message) => {
+            sendMessage(message);
+          }} />
+        <div className="control self-center">
+          <button style={{background: "red", color: "#fff", border: "none", outlineStyle: "none", padding: "5px", borderRadius: "3px", cursor: "pointer"}}
+            className="button is-danger"
+            onClick={() => {
+              localStorage.removeItem("token");
+              navigate("/login");
+            }}
+          >
+            Logout
+          </button>
+        </div>
     </main>
   </div>
     </div>
